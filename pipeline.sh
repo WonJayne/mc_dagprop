3dae0230aea482fbc8e0b6a11cce16bab5983505
WonJayne <46086748+WonJayne@users.noreply.github.com>
2025-06-19 11:10:49 +0200

    Update Windows pipeline to run unittest

---
+++ pipeline.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Delete old build artifacts
+rm -f dist/*
+rm -f build/*
+
+poetry build
+
+for f in dist/mc_dagprop*.whl; do
+    pip install "$f" --force-reinstall
+done
+
+pip install plotly
+
+python test\test_simulator.py
+
